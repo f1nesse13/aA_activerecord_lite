d287798b9e1f599d8b3526c074b9dc81eabadd9a
Joe Pell <jpell3130@gmail.com>
2019-04-15 15:21:16 -0400

    added methods to set up has_many and belongs_to

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -48,17 +48,35 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      foreign_val = send(options.foreign_key)
+      foreign_class = options.model_class
+
+      foreign_class.where(options.primary_key => foreign_val).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+      foreign_val = send(options.primary_key)
+      foreign_class = options.model_class
+
+      foreign_class.where(options.foreign_key => foreign_val)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
