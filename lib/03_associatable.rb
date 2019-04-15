f0cfe1cfb60e260b1aac39129edd8e8a20f31407
Joe Pell <jpell3130@gmail.com>
2019-04-15 15:14:31 -0400

    added options for belongs_to / has_many

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -1,5 +1,5 @@
-require_relative '02_searchable'
-require 'active_support/inflector'
+require_relative "02_searchable"
+require "active_support/inflector"
 
 # Phase IIIa
 class AssocOptions
@@ -10,30 +10,44 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    class_name.constantize.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{name}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.camelcase,
+    }
+    defaults.keys.each do |attr|
+      send("#{attr}=", options[attr] || defaults[attr])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    defaults = {
+      foreign_key: "#{self_class_name.underscore}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.singularize.camelcase,
+    }
+
+    defaults.keys.each do |attr|
+      send("#{attr}=", options[attr] || defaults[attr])
+    end
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
   end
 
   def has_many(name, options = {})
@@ -41,7 +55,7 @@ module Associatable
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    # Wait to implement this in Phase IVa. Modify `belongs_to`, too
   end
 end
 
